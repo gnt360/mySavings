34df2db4f389bd2b6f9c256d4eb63ed07e681ca2
Ayange Felix <ayangefelix8@gmail.com>
2020-10-10 23:32:49 -0700

    add create user account

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\CreateAccountController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,5 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(static function(){
     Route::prefix('users')->name('auth.')->group(static function(){
+        Route::post('/createAccount', [CreateAccountController::class, 'createAccount'])->name('createAccount');
 });
 });

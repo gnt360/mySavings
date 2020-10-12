b142edb1975d10d2891ced12e436d1ab87f1f146
Ayange Felix <ayangefelix8@gmail.com>
2020-10-12 10:31:36 -0700

    add logout functionality

--- app/Http/Controllers/User/UserProfileController.php
+++ app/Http/Controllers/User/UserProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\User;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController;
 use App\Http\Resources\User\ProfileResource;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +17,11 @@ class UserProfileController extends BaseController
         }
 
         return $this->successResponse('profile successfully retrieved', new ProfileResource($user));
+    }
 
+      public function logOut(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return $this->successResponse('You have been successfully logged out!');
     }
 }
